e3f27d1ec94493f224360f72272ad3d469d19605
RyzenMu <muruga13in2022@gmail.com>
2024-03-18 20:12:46 +0530

    spring boot

---
+++ helloin/src/test/java/com/helloout/helloin/HelloinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloout.helloin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
